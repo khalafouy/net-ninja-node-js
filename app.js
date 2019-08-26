d5b5bc3921786dc0323ec58788fc7899c1c52639
Mohamed Khalaf <mohamedkhalaf0912@gmail.com>
2019-08-26 08:40:44 +0200

    using require , module exports

--- app.js
+++ app.js
@@ -1,34 +1,4 @@
+// example about export module
+let counter = require('./count');
 
-// Example on Global object 
-let time = 0;
-let timer = setInterval(function () {
-    time++;
-    console.log(time + ' seconds passed ');
-    if(time > 5)
-    clearInterval(timer);
-}, 1000);
-
-
-// Example about Normal function & function expression 
-
-// normal function 
-function sayHi()
-{
-    console.log("Welcome Man ");
-}
-sayHi();
-
-// function expression
-var sayBye = function sayHi()
-{
-    console.log("Bye Man ");
-}
-sayBye();
-// Also function expression used for send function as paramters
-
-function calFunction(fun)
-{
-    fun();
-}
-calFunction(sayBye);
-
+counter(['p1','p2','p3']);
---
+++ count.js
@@ -0,0 +1,5 @@
+var counter = function(arr){
+    console.log('Array length is '+arr.length);
+}
+
+module.exports = counter;
