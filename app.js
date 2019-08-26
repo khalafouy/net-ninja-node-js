8406ddb3ec991c1c28f530efb9d046f7a1c02438
Mohamed Khalaf <mohamedkhalaf0912@gmail.com>
2019-08-26 08:22:40 +0200

    diffrence between normal / expression function

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
+
+// Example on Global object 
 let time = 0;
 let timer = setInterval(function () {
     time++;
     console.log(time + ' seconds passed ');
-    if(time > 100)
+    if(time > 5)
     clearInterval(timer);
 }, 1000);
+
+
+// Example about Normal function & function expression 
+
+// normal function 
+function sayHi()
+{
+    console.log("Welcome Man ");
+}
+sayHi();
+
+// function expression
+var sayBye = function sayHi()
+{
+    console.log("Bye Man ");
+}
+sayBye();
+// Also function expression used for send function as paramters
+
+function calFunction(fun)
+{
+    fun();
+}
+calFunction(sayBye);
+
