b932d9ee92f740553a9869e212bba09781e4954a
Mohamed Khalaf <mohamedkhalaf0912@gmail.com>
2019-08-28 21:31:10 +0200

    describes core module of fs & delete files & create/delete dir

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
-// events module exist by default in node
-var events = require('events');
+var fs = require('fs');
+// example of delete file 
 
-var Eventemitter = new events.EventEmitter();
+fs.unlink('read.txt',function(){
 
-Eventemitter.on('sayHello', function (name) {
-    console.log('Hello ' + name);
-})
+});
 
-Eventemitter.emit('sayHello', 'Khalaf');
-
-var util = require('util');
-
-var Person = function (name) {
-    this.name = name;
-}
-
-util.inherits(Person, events.EventEmitter);
-
-var person1 = new Person('aya');
-var person2 = new Person('basma');
-var person3 = new Person('mama');
-
-var persons = [person1, person2, person3];
-
-persons.forEach(person => {
-    person.on('sayHello', function () {
-        console.log('Hello ' + this.name);
+// example of create dir 
+fs.mkdir('stuff',function(){
+    fs.rmdir('stuff',function(){
+        
     });
 });
-console.log(person1);
-person1.emit('sayHello');
