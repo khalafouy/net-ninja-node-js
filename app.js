7419875ccf2f7f519feb1a353bf2477a2dfa4f22
Mohamed Khalaf <mohamedkhalaf0912@gmail.com>
2019-09-08 08:33:02 +0200

    writes readable stream file

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-// http core  module exist by default in node
-var http = require('http');
+// buffer & streams 
+// buffer is temprory storage used to slice data to bits and transfer it to client 
+// increase preformance rather than waiting load whole data 
 
-var server = http.createServer(function(request,response){
-    console.log(request.url);
-    response.writeHead(200,{'Content-type':'text\plain'});
-    response.end("Welcome Man");
+var fs = require('fs');
+
+var readableStream = fs.createReadStream('text.txt', 'utf8');
+
+readableStream.on('data', function (chunk) {
+    console.log('New Chunk receievd');
+    console.log(chunk);
 });
-
-server.listen(3000,'127.0.0.1');
-console.log("server run");
