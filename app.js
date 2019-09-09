1dad5e748c3f137f88a52d0f8f1891ea63c8df98
Mohamed Khalaf <mohamedkhalaf0912@gmail.com>
2019-09-09 10:00:21 +0200

    how to use pipe

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-// buffer & streams 
-// buffer is temprory storage used to slice data to bits and transfer it to client 
-// increase preformance rather than waiting load whole data 
-
+// using pipe rather listen on readableStream then write it 
 var fs = require('fs');
 
 var readableStream = fs.createReadStream('readMe.txt', 'utf8');
 var writeableStream = fs.createWriteStream('writeMe.txt');
 
-readableStream.on('data', function (chunk) {
-    console.log('New Chunk receievd');
-    console.log(chunk);
-    writeableStream.write(chunk);
-});
+readableStream.pipe(writeableStream);
