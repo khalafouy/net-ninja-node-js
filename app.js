30104db0ffdb4871275fc43f4585ff6d9c274ba4
Mohamed Khalaf <mohamedkhalaf0912@gmail.com>
2019-09-15 23:15:25 +0200

    makes routing file

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-// return response as json
+// basic routing
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(request,response){
-    console.log(request.url);
-    response.writeHead(200,{'Content-type':'application\json'});
-    var object = {name:"khalaf",job:"developer",age:12};
-    response.end(JSON.stringify(object));
+    if(request.url === '/home' || request.url === '/')
+    {
+        response.writeHead(200,{'Content-type':'text\html'});
+        fs.createReadStream('home.html').pipe(response);
+    }
+    else if(request.url === '/contact')
+    {
+        response.writeHead(200,{'Content-type':'text\html'});
+        fs.createReadStream('contact.html').pipe(response);
+    }
+    else if(request.url === '/api')
+    {
+        response.writeHead(200,{'Content-type':'application\json'});
+        var object = [{name:"khalaf",job:"developer",age:12},{name:"basma",job:"doctor",age:131}];
+        response.end(JSON.stringify(object));   
+    }
+    else
+    { 
+       response.writeHead(404,{"Content-Type":'text\html'}); 
+       fs.createReadStream(__dirname+'/404.html').pipe(response);
+    }
 });
 
 server.listen(3000,'127.0.0.1');
