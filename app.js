7025258cd28dc995e4d14d61290df6ae022e3d14
Mohamed Khalaf <mohamedkhalaf0912@gmail.com>
2019-09-29 22:02:35 +0200

    use express & ejs

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine','ejs');
+
 
 app.get('/',function(req,res){
-    res.send('home screen');
+    res.sendFile(__dirname+'/home.html');
 });
 
 app.get('/contact',function(req,res){
-    res.send('contact screen');
+    res.sendFile(__dirname+'/contact.html');
+});
+
+
+app.get('/profile/:name',function(req,res){
+    res.render('profile',{name:req.params.name});
 });
 
 app.listen(3000);
